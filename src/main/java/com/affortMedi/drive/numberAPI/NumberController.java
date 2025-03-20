1b157b2a0c417dc5366b808927a6f8dfc3044038
KaVin S <kavinadithya3@gmail.com>
2025-03-20 11:24:39 +0530

    Fibonacci series printing API created

---
+++ src/main/java/com/affortMedi/drive/numberAPI/NumberController.java
@@ -0,0 +1,28 @@
+package com.affortMedi.drive.numberAPI;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class NumberController {
+	
+	@Autowired
+	private NumberService num;
+	
+	@GetMapping("/fibo/{n}")
+	public List<Integer> fiboNumber(@PathVariable Integer n) {
+		return num.fibo(n);
+	}
+	
+	@GetMapping("/even/{start}/{end}")
+	public List<Integer> evenNumbers(@PathVariable Integer start, @PathVariable Integer end) {
+		return null;
+	}
+	
+}
---
+++ src/main/java/com/affortMedi/drive/numberAPI/NumberService.java
@@ -0,0 +1,35 @@
+package com.affortMedi.drive.numberAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class NumberService {
+	public List<Integer> fibo(int n) {
+		List<Integer> series = new ArrayList<>();
+		
+		series.add(0);
+		series.add(1);
+		
+		int a = 0;
+		int b = 1;
+	
+		
+		for (int i = 2; i <= n; i++) {
+			int c = a + b;
+			a = b;
+			b = c;
+			
+			series.add(b);
+		}
+		
+		return series;
+		
+	}
+	
+//	public List<Integer> evenNumbers(int start, int end) {
+//		List<Integer> list = new 
+//	}
+}
