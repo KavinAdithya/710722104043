9f61ad894f015c83664899bf0c6d16b3c410949a
KaVin S <kavinadithya3@gmail.com>
2025-03-20 10:39:06 +0530

    Maven spring Boot Project Set up completed Using H2 Databbase for social media all dependences are included

---
+++ src/main/java/com/affortMedi/drive/AffordMediSocialApplication.java
@@ -0,0 +1,13 @@
+package com.affortMedi.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AffordMediSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AffordMediSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/affortMedi/drive/AffordMediSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.affortMedi.drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AffordMediSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
